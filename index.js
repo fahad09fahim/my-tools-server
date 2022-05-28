1352613544409b63af3e0e0279a81f58a0352810
fahad09fahim <anik.fahim07@gmail.com>
2022-05-28 12:51:21 +0600

    admin role data added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -16,11 +17,28 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+function verifyJwt(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden Access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run() {
   try {
     await client.connect();
     const toolsCollection = client.db("mytools").collection("tools");
     const reviewCollection = client.db("mytools").collection("reviews");
+    const orderCollection = client.db("mytools").collection("order");
+    const usersCollection = client.db("mytools").collection("users");
 
     // load tools to client
     app.get("/tools", async (req, res) => {
@@ -42,19 +60,80 @@ async function run() {
       res.send(result);
     });
     app.post("/order", async (req, res) => {
-      const order = req.body;
+      const orders = req.body;
       const query = {
-        name: order.productName,
-        available: order.quantity,
+        name: orders.productName,
+        buyerEmail: orders.buyerEmail,
+        available: orders.quantity,
       };
-      const exists = await toolsCollection.findOne(query);
+      const exists = await orderCollection.findOne(query);
       if (exists) {
         return res.send({ success: false, order: exists });
       }
-      const result = await toolsCollection.insertOne(order);
+      const result = await orderCollection.insertOne(orders);
 
       res.send({ success: true, result });
     });
+    app.get("/order", async (req, res) => {
+      const user = req.query.buyerEmail;
+      const query = { user: user };
+      const orders = await orderCollection.find(query).toArray();
+      res.send(orders);
+    });
+
+    app.get("/user", verifyJwt, async (req, res) => {
+      const user = await usersCollection.find().toArray();
+      res.send(user);
+    });
+
+    app.put("/user/admin/:email", verifyJwt, async (req, res) => {
+      const email = req.params.email;
+      const requester = req.decoded.email;
+      const requesterAccount = await usersCollection.findOne({
+        email: requester,
+      });
+      if (requesterAccount.role === "admin") {
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: "admin" },
+        };
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      } else {
+        res.status(403).send({ message: "Forbidden" });
+      }
+    });
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET);
+      res.send({ result, token });
+    });
+
+    // app.post("/order", async (req, res) => {
+    //   const order = req.body;
+    //   const query = {
+    //     name: order.productName,
+    //     available: order.quantity,
+    //   };
+    //   const exists = await toolsCollection.findOne(query);
+    //   if (exists) {
+    //     return res.send({ success: false, order: exists });
+    //   }
+    //   const result = await toolsCollection.insertOne(order);
+
+    //   res.send({ success: true, result });
+    // });
   } finally {
   }
 }
